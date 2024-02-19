43102b25cc1ab972603f46ffd69f9e59766660d6
William Dillon <wmdillon@me.com>
2024-02-19 14:05:11 -0500

    added a benchmark

--- rsv_test.go
+++ rsv_test.go
@@ -1,7 +1,12 @@
 package rsv
 
 import (
+	"bufio"
 	"bytes"
+	"math/rand"
+	"os"
+	"slices"
+	"strings"
 	"testing"
 )
 
@@ -16,7 +21,10 @@ var TestCases = []TestCase{
 			[]byte("Hello"),
 			[]byte("🌎"),
 		},
-		b: []byte{72, 101, 108, 108, 111, ValueTerminator, 240, 159, 140, 142, ValueTerminator, RowTerminator},
+		b: []byte{72, 101, 108, 108, 111,
+			ValueTerminator,
+			240, 159, 140, 142,
+			ValueTerminator, RowTerminator},
 	},
 	{
 		s: Record{},
@@ -46,5 +54,39 @@ func TestRSV(t *testing.T) {
 		} else if want, got := len(testcase.s), len(records[0]); want != got {
 			t.Fatalf("error for test case %d: wanted %d values; got %d\n", i, want, got)
 		}
+	}
+}
+
+var TheWords = func() []string {
+	f, err := os.Open("/usr/share/dict/words")
+	if err != nil {
+		panic("error opening words file: " + err.Error())
+	}
+	defer f.Close()
+	results := make([]string, 0)
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		if line := strings.TrimSpace(scanner.Text()); len(line) > 0 {
+			results = append(results, line)
+		}
+	}
+	slices.Sort(results)
+	return results
+}()
+
+var TheWordRecords = func() []Record {
+	results := make([]Record, 0, len(TheWords))
+	for _, word := range TheWords {
+		results = append(results, Record{[]byte(word)})
+	}
+	return results
+}()
+
+func RandomRecord() Record { return TheWordRecords[rand.Intn(len(TheWordRecords))] }
+
+func BenchmarkWrite(b *testing.B) {
+	writer := NewWriter(new(bytes.Buffer))
+	for i := 0; i < b.N; i++ {
+		writer.Write(RandomRecord())
 	}
 }
